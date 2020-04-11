a7dfb9b81bafe03fef8c038eb06506bfed08abc0
Egill Ragnarsson <egr4@hi.is>
2020-04-11 17:26:28 +0000

    fix admin password

--- sql/insert.sql
+++ sql/insert.sql
@@ -1,5 +1,5 @@
 INSERT INTO users
     (username, email, password, admin)
 VALUES
-    ('admin', 'admin@admin.is', '$2b$11$VVnEpbR.Gqe/1WJoVrLaouiLwKEyMJKw.1aST6gy1C3ILD.oD/UXG', true);
+    ('admin', 'admin@admin.is', '12345678', true);
 
